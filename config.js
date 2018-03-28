133c3b60f2289ae4ddabc918b3fe5a2e623dc985
Jonathan Keebler <jonathan@keebler.net>
2018-03-28 01:03:39 -0400

    Notify Smartthings when the online status changes

--- config.js
+++ config.js
@@ -2,9 +2,11 @@ module.exports = {
 	"ips": ["192.168.86.229"],
 	"min_time_between_checks": 1000,
 	"min_time_between_online_checks": 5000,
+	"online_threshold": 0,
+	"offline_threshold": 5000,
 	"smartthings": {
 		"ide": "https://graph-na04-useast2.api.smartthings.com",
 		"access_token": process.env.ACCESS_TOKEN || "PASTE YOUR ACCESS TOKEN",
-		"app_id": process.env.ACCESS_TOKEN || "PASTE YOUR APP ID" 
+		"app_id": process.env.APP_ID || "PASTE YOUR APP ID" 
 	}
 };
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const ping = require("ping");
+const ping = require("ping"),
+	request = require("superagent");
 
 const config = require("./config.js");
 
@@ -6,6 +7,7 @@ let status = {};
 config.ips.forEach(function(ip)
 {
 	status[ip] = {
+		online: undefined,
 		last_online: new Date(0),
 		last_offline: new Date(0)
 	};
@@ -17,12 +19,32 @@ setInterval(()=>{
 
 }, 1000);
 
+const notify_ide = (ip, alive)=>{
+
+	console.log("Notifying IDE for " + ip);
+	request
+		.get(`${config.smartthings.ide}/api/smartapps/installations/${config.smartthings.app_id}/statechanged/${alive ? "online": "offline"}`)
+		.query({
+			access_token: config.smartthings.access_token,
+			ipadd: ip
+		})
+		.end((err, res)=>{
+
+			if(res && res.status == 200)
+			{
+				status[ip].online = alive;
+			}
+		});
+
+};
+
 const check_status = () =>{
 
 	let checks = Object.keys(status).map((ip)=>{
 
 		if(new Date() - status[ip].last_online > config.min_time_between_online_checks)
 		{
+			console.log(`Checking ${ip}`);
 			return ping.promise.probe(ip, {
 				timeout: 1
 			});
@@ -36,10 +58,22 @@ const check_status = () =>{
 			if(!result) return;
 
 			status[result.host][`last_${result.alive ? "online": "offline"}`] = new Date();
-			
-		});
 
-		console.log(status);
+			if(status[result.host].online === undefined)
+			{
+				notify_ide(result.host, result.alive);
+			}
+			else if(result.alive && !status[result.host].online)
+			{
+				// Looks like you might be going online
+				notify_ide(result.host, result.alive);
+			}
+			else if(!result.alive && status[result.host].online)
+			{
+				// Looks like you might be going offline
+				notify_ide(result.host, result.alive);
+			}
+		});
 
 		setTimeout(check_status, config.min_time_between_checks);
 
